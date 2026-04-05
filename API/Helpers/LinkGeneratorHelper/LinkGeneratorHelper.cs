691f689af2c218392c9198b6f4e8136ee0d115de
muhmdramah <mohammedrammah83@gmail.com>
2026-04-05 15:05:29 +0200

    refactor: Refactor LinkGeneratorHelper to improve HttpContext usage
    - Store HttpContext in a local variable to avoid repeated access
    - Improve readability and consistency

--- API/Helpers/LinkGeneratorHelper/LinkGeneratorHelper.cs
+++ API/Helpers/LinkGeneratorHelper/LinkGeneratorHelper.cs
@@ -22,21 +22,22 @@ namespace API.Helpers.LinkGeneratorHelper
                 return string.Empty; // Handle background task scenarios where HTTP context is null
 
             // Generates an absolute URL (e.g., https://skinet.com/api/orders/5)
+            var context = _httpContextAccessor.HttpContext;
             return new
             {
-                self = _linkGenerator.GetUriByName(_httpContextAccessor.HttpContext,
+                self = _linkGenerator.GetUriByName(context,
                     endpointName: "GetById", // This must match the Name="" on your Controller endpoint
                     values: new { id = entityId }),
 
-                create = _linkGenerator.GetUriByName(_httpContextAccessor.HttpContext,
+                create = _linkGenerator.GetUriByName(context,
                     endpointName: "Create",
                     values: new { }),
 
-                update = _linkGenerator.GetUriByName(_httpContextAccessor.HttpContext,
+                update = _linkGenerator.GetUriByName(context,
                     endpointName: "Update",
                     values: new { id = entityId }),
 
-                delete = _linkGenerator.GetUriByName(_httpContextAccessor.HttpContext,
+                delete = _linkGenerator.GetUriByName(context,
                     endpointName: "Delete",
                     values: new { id = entityId }),
             };
