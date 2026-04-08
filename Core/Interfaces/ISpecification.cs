f2852776b910bee046f01e81d149b7a6246f7b0b
muhmdramah <mohammedrammah83@gmail.com>
2026-04-08 16:17:02 +0200

    Feat: Implement specification pattern for query filtering
    - Add ISpecification<T> interface with Criteria expression
    - Add BaseSpecification<T> abstract implementation
    - Add SpecificationEvaluator<T> to apply specifications to queries
    - Constrain generic types to BaseEntity where applicable

---
+++ Core/Interfaces/ISpecification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.Interfaces
+{
+    public interface ISpecification<T> where T : class
+    {
+        Expression<Func<T, bool>> Criteria { get; }
+    }
+}
---
+++ Infrastructure/Repositories/BaseSpecification.cs
@@ -0,0 +1,16 @@
+﻿using Core.Interfaces;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repositories
+{
+    public class BaseSpecification<T> : ISpecification<T> where T : class
+    {
+        private readonly Expression<Func<T, bool>> _criteria;
+
+        public BaseSpecification(Expression<Func<T, bool>> criteria)
+        {
+            _criteria = criteria;
+        }
+        public Expression<Func<T, bool>> Criteria => _criteria;
+    }
+}
