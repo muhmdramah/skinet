b42387d8aba8518f24b7407632fc2b2d672d5e87
muhmdramah <mohammedrammah83@gmail.com>
2026-04-08 16:50:28 +0200

    Feat: Add specification pattern foundation
    - Add ISpecification<T> interface for defining query criteria
    - Add BaseSpecification<T> as concrete implementation of the specification pattern

---
+++ Core/Specifications/ProductSpecification.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.Specifications
+{
+    public class ProductSpecification : BaseSpecification<Product>
+    {
+        public ProductSpecification(string? brand, string? type) : base(criteria: x => 
+            (string.IsNullOrWhiteSpace(brand) || x.ProductBrand == brand) &&
+            (string.IsNullOrWhiteSpace(type) || x.ProductType == type)
+        )
+        {
+            
+        }
+    }
+}
